af30a189b8a9866e36896e7ea460424863fd54bd
Richard <richard.wigley@gmail.com>
2021-07-23 13:11:18 +0100

    Add custom colors to base and tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,22 @@ module.exports = {
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      pink: {
+        100: 'var(--color-pink-100)',
+      },
+      cyan: {
+        100: 'var(--color-cyan-100)',
+        200: 'var(--color-cyan-200)',
+      },
+      gray: {
+        100: 'var(--color-gray-100)',
+      },
+    },
     extend: {},
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
